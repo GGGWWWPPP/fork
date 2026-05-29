a2c80dccf870638720401beba4cc85fa82914e0f
Admin <admin@nodeconnect.tech>
2026-05-29 13:49:45 +1000

    Update HWID migration

--- nodeconnect-core/app/db/migrations/versions/a1b2c3d4e5f6_add_hwid.py
+++ nodeconnect-core/app/db/migrations/versions/a1b2c3d4e5f6_add_hwid.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a1b2c3d4e5f6'
-down_revision = 'fe7796f840a4'  # Latest revision based on the list
+down_revision = '2b231de97dc3'
 branch_labels = None
 depends_on = None
 
